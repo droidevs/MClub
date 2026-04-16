f8690fb63a1482e160d4382ae99131c6f92f0538
Achraf EL-BIYANI <achrafelbiyani.297@gmail.com>
2026-04-16 14:21:40 +0200

    refactor EventRatingFactoryMapper to use default method for entity creation

--- src/main/java/io/droidevs/mclub/mapper/EventRatingFactoryMapper.java
+++ src/main/java/io/droidevs/mclub/mapper/EventRatingFactoryMapper.java
@@ -2,18 +2,18 @@ package io.droidevs.mclub.mapper;
 
 import io.droidevs.mclub.domain.EventRating;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
+/**
+ * Factory-style mapper.
+ *
+ * <p>MapStruct cannot generate no-arg mapping methods; for pure "create empty entity" use a default
+ * method so construction stays out of services and remains injectable/testable.
+ */
 @Mapper(componentModel = "spring", config = CentralMapperConfig.class)
 public interface EventRatingFactoryMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "event", ignore = true)
-    @Mapping(target = "student", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "rating", ignore = true)
-    @Mapping(target = "comment", ignore = true)
-    EventRating create();
+    default EventRating create() {
+        return new EventRating();
+    }
 }
 
